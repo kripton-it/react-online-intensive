edf837213d608a32e5790e9336a7f6ea6d77a1e4
Mikhail Shardin <kriptonit1988@gmail.com>
2019-06-01 10:56:59 +0500

    ДЗ №4

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -3,20 +3,26 @@ import React, { Component } from "react";
 //Instruments
 import Styles from "./styles.m.css";
 
+// Components
+import { Consumer } from "../../HOC";
+
 export default class Composer extends Component {
   render() {
     const { composer } = Styles;
-    const { avatar, currentUserFirstName } = this.props;
     return (
-      <section className={composer}>
-        <img src={avatar} alt="Avatar" />
-        <form action="">
-          <textarea
-            placeholder={`What's on your mind, ${currentUserFirstName}?`}
-          />
-          <input type="submit" value="Post" />
-        </form>
-      </section>
+      <Consumer>
+        {({ avatar, currentUserFirstName }) => (
+          <section className={composer}>
+            <img src={avatar} alt="Avatar" />
+            <form action="">
+              <textarea
+                placeholder={`What's on your mind, ${currentUserFirstName}?`}
+              />
+              <input type="submit" value="Post" />
+            </form>
+          </section>
+        )}
+      </Consumer>
     );
   }
 }
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -4,20 +4,27 @@ import moment from "moment";
 //Instruments
 import Styles from "./styles.m.css";
 
+// Components
+import { Consumer } from "../../HOC";
+
 export default class Post extends Component {
   render() {
     const { post } = Styles;
-    const { avatar, currentUserFirstName, currentUserLastName } = this.props;
-
-    const currentUserFullName = `${currentUserFirstName} ${currentUserLastName}`;
 
     return (
-      <section className={post}>
-        <img src={avatar} />
-        <a href="#">{currentUserFullName}</a>
-        <time>{moment().format("MMMM D h:mm:ss a")}</time>
-        <p>Howdy!</p>
-      </section>
+      <Consumer>
+        {({ avatar, currentUserFirstName, currentUserLastName }) => {
+          const currentUserFullName = `${currentUserFirstName} ${currentUserLastName}`;
+          return (
+            <section className={post}>
+              <img src={avatar} />
+              <a href="#">{currentUserFullName}</a>
+              <time>{moment().format("MMMM D h:mm:ss a")}</time>
+              <p>Howdy!</p>
+            </section>
+          );
+        }}
+      </Consumer>
     );
   }
 }
