70e1455e0838f9f0f7ee6811493fc6897a8f30e1
Mikhail Shardin <kriptonit1988@gmail.com>
2019-06-09 16:23:05 +0500

    Добавление нового поста

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { func } from 'prop-types';
 
 //Instruments
 import Styles from './styles.m.css';
@@ -7,8 +8,45 @@ import Styles from './styles.m.css';
 import { Consumer } from './../../HOC/with-profile';
 
 export default class Composer extends Component {
+    static propTypes = {
+        createPost: func.isRequired,
+    }
+
+    constructor() {
+        super();
+        this._updateComment = this._updateComment.bind(this);
+        this._submitComment = this._submitComment.bind(this);
+    }
+
+    state = {
+        comment: '',
+    }
+
+    _updateComment(event) {
+        this.setState({
+            comment: event.target.value,
+        });
+    }
+
+    _submitComment(event) {
+        event.preventDefault();
+
+        const { comment } = this.state;
+        if (!comment) {
+            return;
+        }
+
+        const { createPost } = this.props;
+        createPost(comment);
+
+        this.setState({
+            comment: '',
+        });
+    }
+
     render() {
         const { composer } = Styles;
+        const { comment } = this.state;
 
         return (
             <Consumer>
@@ -18,9 +56,13 @@ export default class Composer extends Component {
                             alt = 'Avatar'
                             src = { avatar }
                         />
-                        <form action = ''>
+                        <form
+                            action = ''
+                            onSubmit = { this._submitComment }>
                             <textarea
                                 placeholder = { `What's on your mind, ${currentUserFirstName}?` }
+                                value = { comment }
+                                onChange = { this._updateComment }
                             />
                             <input
                                 type = 'submit'
--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 // Instruments
 import Styles from './styles.m.css';
+import { getUniqueID } from '../../instruments';
 
 // Components
-import Composer from './../Composer';
-import Post from './../Post';
+import Composer from '../Composer';
+import Post from '../Post';
 import StatusBar from '../StatusBar';
 import Spinner from '../Spinner';
 
 export default class Feed extends Component {
+    constructor() {
+        super();
+        this._createPost = this._createPost.bind(this);
+    }
+
     state = {
         posts: [
             {
-                id: 123,
+                id:      123,
                 comment: 'Hi there',
                 created: 1526825076849,
             },
             {
-                id: 253,
+                id:      253,
                 comment: 'Приветик ✋',
                 created: 1526825076849,
             },
         ],
         isSpinning: true,
+    }
+
+    _createPost(comment) {
+        const post = {
+            id:      getUniqueID(),
+            created: moment.utc(),
+            comment,
+        };
+
+        this.setState(({ posts }) => ({
+            posts: [ post, ...posts ],
+        }));
     }
 
     render() {
@@ -40,7 +59,7 @@ export default class Feed extends Component {
             <section className = { feed }>
                 <Spinner isSpinning = { isSpinning } />
                 <StatusBar />
-                <Composer />
+                <Composer createPost = { this._createPost } />
                 { postsJSX }
             </section>
         );
