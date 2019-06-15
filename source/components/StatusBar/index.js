efdcdbf728433f94622b605cb9bfb01efb6776ff
Mikhail Shardin <kriptonit1988@gmail.com>
2019-06-15 12:21:13 +0500

    ДЗ №7

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -4,28 +4,26 @@ import React, { Component } from 'react';
 import Styles from './styles.m.css';
 
 // Components
-import { Consumer } from './../../HOC/with-profile';
+import { withProfile } from './../../HOC/with-profile';
 
+@withProfile
 export default class StatusBar extends Component {
     render() {
         const { statusBar } = Styles;
+        const { avatar, currentUserFirstName, currentUserLastName } = this.props;
 
         return (
-            <Consumer>
-                {({ avatar, currentUserFirstName, currentUserLastName }) => (
-                    <section className = { statusBar }>
-                        <button>
-                            <img
-                                alt = 'Avatar'
-                                src = { avatar }
-                            />
-                            <span>{currentUserFirstName}</span>
-                            &nbsp;
-                            <span>{currentUserLastName}</span>
-                        </button>
-                    </section>
-                )}
-            </Consumer>
+            <section className = { statusBar }>
+                <button>
+                    <img
+                        alt = 'Avatar'
+                        src = { avatar }
+                    />
+                    <span>{currentUserFirstName}</span>
+                    &nbsp;
+                    <span>{currentUserLastName}</span>
+                </button>
+            </section>
         );
     }
 }
