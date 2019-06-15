13a51a4b3867f15f44f711aab233dc5f33392321
Mikhail Shardin <kriptonit1988@gmail.com>
2019-06-15 17:33:37 +0500

    Online статус с помощью socket

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
+import cx from 'classnames';
 
 // Instruments
 import Styles from './styles.m.css';
+import { socket } from '../../socket/init';
 
 // Components
 import { withProfile } from './../../HOC/with-profile';
 
 @withProfile
 export default class StatusBar extends Component {
+    state = {
+        isOnline: false,
+    };
+
+    componentDidMount() {
+        socket.on('connect', () => {
+            this.setState({
+                isOnline: true,
+            });
+        });
+
+        socket.on('disconnect', () => {
+            this.setState({
+                isOnline: false,
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        socket.removeListener('connect');
+        socket.removeListener('disconnect');
+    }
+
     render() {
-        const { statusBar } = Styles;
+        const { statusBar, status, online, offline } = Styles;
         const { avatar, currentUserFirstName, currentUserLastName } = this.props;
+        const { isOnline } = this.state;
+
+        const statusStyle = cx(status, {
+            [ online ]:  isOnline,
+            [ offline ]: !isOnline,
+        });
+
+        const statusMessage = isOnline ? 'Online' : 'Offline';
 
         return (
             <section className = { statusBar }>
+                <div className = { statusStyle }>
+                    <div>{ statusMessage }</div>
+                    <span />
+                </div>
                 <button>
                     <img
                         alt = 'Avatar'
