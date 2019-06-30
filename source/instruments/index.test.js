e6baaffd058b0b4023f89e18cb99d8a67760fbb1
Mikhail Shardin <kriptonit1988@gmail.com>
2019-06-30 15:08:08 +0500

    Тестирование функций delay и getUniqueID

--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -1,7 +1,11 @@
 // Core
-import { sum } from './';
+import { sum, delay, getUniqueID } from './';
 
-describe('instruments:', () => {
+// jest.setTimeout(15000);
+// дефолтный таймаут = 5000 мс
+// изменить при необходимости тестирования асинхронных функций с большой задержкой
+
+describe('sum:', () => {
     test('sum function should be a function', () => {
         expect(sum).toBeInstanceOf(Function);
     });
@@ -20,3 +24,37 @@ describe('instruments:', () => {
     });
 });
 
+describe('delay:', () => {
+    test('delay function should be a function', () => {
+        expect(delay).toBeInstanceOf(Function);
+    });
+
+    test('delay function should return a resolved promise', async () => {
+        await expect(delay()).resolves.toBeUndefined();
+    });
+
+    /*test('delay function should return a resolved promise', async () => {
+        await expect(delay(15000)).resolves.toBe('A resolved promise 🚦');
+    });
+
+    test('delay function should return a resolved promise', async () => {
+        await expect(delay()).resolves.toBe('A resolved promise 🚦');
+    });*/
+});
+
+describe('getUniqueID:', () => {
+    test('getUniqueID function should be a function', () => {
+        expect(getUniqueID).toBeInstanceOf(Function);
+    });
+
+    test('getUniqueID function should throw, when called with non-number type as argument', () => {
+        expect(() => getUniqueID('hello')).toThrow();
+    });
+
+    test('getUniqueID function should return a string of a desired given length', () => {
+        expect(typeof getUniqueID()).toBe('string');
+        expect(getUniqueID()).toHaveLength(15);
+        expect(getUniqueID(5)).toHaveLength(5);
+        expect(getUniqueID(10)).toHaveLength(10);
+    });
+});
