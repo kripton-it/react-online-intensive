3eb76fc7cff20e150e0bb57fa4d0954e5016ff18
Mikhail Shardin <kriptonit1988@gmail.com>
2019-07-01 00:17:19 +0500

    ДЗ №12

--- source/components/Composer/Composer.test.js
+++ source/components/Composer/Composer.test.js
@@ -7,8 +7,13 @@ import { Composer } from './';
 
 // configure({ adapter: new Adapter() });
 
+//Instruments
+import avatar from '../../theme/assets/lisa.png';
+
 const props = {
-    createPost: jest.fn(),
+    createPost:           jest.fn(),
+    currentUserFirstName: 'Lisa',
+    avatar,
 };
 
 const comment = 'Hello, World!';
@@ -25,6 +30,8 @@ const result = mount(<Composer { ...props } />);
 
 const _submitCommentSpy = jest.spyOn(result.instance(), '_submitComment');
 const _handleFormSubmitSpy = jest.spyOn(result.instance(), '_handleFormSubmit');
+const _updateCommentSpy = jest.spyOn(result.instance(), '_updateComment');
+const _submitOnEnterSpy = jest.spyOn(result.instance(), '_submitOnEnter');
 
 describe('component <Composer>:', () => {
     test('should have 1 <section> element', () => {
@@ -74,6 +81,7 @@ describe('component <Composer>:', () => {
                 value: comment,
             },
         });
+        // expect(_updateCommentSpy).toHaveBeenCalledTimes(1);
         expect(result.state()).toEqual(updatedState);
         expect(result.find('textarea').text()).toBe(comment);
     });
@@ -85,13 +93,64 @@ describe('component <Composer>:', () => {
     });
 
     test('createPost method should be invoked once after form submission', () => {
+        result.setState({
+            comment,
+        });
+        result.find('form').simulate('submit');
         // expect(props.createPost.mock.calls.length).toBe(1);
         // expect(props.createPost).toHaveBeenCalled();
         expect(props.createPost).toHaveBeenCalledTimes(1);
     });
 
     test('_submitComment and _handleFormSubmit class methods should be invoked once after form submission', () => {
+        result.find('form').simulate('submit');
         expect(_submitCommentSpy).toHaveBeenCalledTimes(1);
         expect(_handleFormSubmitSpy).toHaveBeenCalledTimes(1);
     });
+
+    test('<img> element should have src attribute from props', () => {
+        expect(result.find('img').prop('src')).toBe(avatar);
+    });
+
+    test('<textarea> element should have placeholder attribute from props', () => {
+        expect(result.find('textarea').prop('placeholder')).toBe('What\'s on your mind, Lisa?');
+    });
+
+    test('_updateComment class method should be invoked after textarea change', () => {
+        result.find('textarea').simulate('change', {
+            target: {
+                value: comment,
+            },
+        });
+        expect(_updateCommentSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('_submitOnEnter class method should be invoked after key press in textarea', () => {
+        result.find('textarea').simulate('keyPress');
+        expect(_submitOnEnterSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('_submitOnEnter class method should not invoke _submitComment method after not-Enter key press in textarea', () => {
+        result.find('textarea').simulate('keyPress', {
+            key: 'Escape',
+        });
+        expect(_submitCommentSpy).not.toHaveBeenCalled();
+    });
+
+    test('_submitOnEnter class method should invoke _submitComment method after Enter key press in textarea', () => {
+        result.find('textarea').simulate('keyPress', {
+            key: 'Enter',
+        });
+        expect(_submitCommentSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('_submitComment and class method should not invoke createPost after form submission, if the comment is empty', () => {
+        result.setState({
+            comment: '',
+        });
+        result.find('form').simulate('submit');
+        expect(props.createPost).not.toHaveBeenCalled();
+    });
 });
+
+afterEach(() => jest.clearAllMocks());
